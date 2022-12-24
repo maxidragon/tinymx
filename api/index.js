18569f8a91b7f3223ddb8fe2b5f92df7402cb3ca
Bartłomiej Strama <80683535+BaderBC@users.noreply.github.com>
2022-12-24 23:25:07 +0100

    dotenv better version

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
+require('dotenv')
+
 const express = require('express')
 const cors = require('cors')
-const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 5000
